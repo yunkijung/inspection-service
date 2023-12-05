85819c27184d97836d1fafa0ca6ea809a20635d3
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2023-12-05 00:33:38 -0500

    init

---
+++ src/test/java/com/loglrs/inspectionservice/InspectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loglrs.inspectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
